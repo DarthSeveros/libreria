982240f39a2605ed9ca00965793a89d4af1b416b
José Velásquez <joseivecas@gmail.com>
2022-06-06 21:57:16 -0400

    Input variables

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-from tkinter import * 
+from tkinter import *
+from tkinter import ttk
+
+def login_clicked():
+    print(textInputUser.get())
 
 root = Tk()
 root.geometry("600x400")
-frame = Frame(root)
-frame.pack()
- 
-leftframe = Frame(root)
-leftframe.pack(side=LEFT)
- 
-rightframe = Frame(root)
-rightframe.pack(side=RIGHT)
- 
-label = Label(frame, text = "Ingresar")
-label.pack()
-
-my_entry = Entry(frame, width = 20)
-my_entry.insert(0,'Usuario')
-my_entry.pack(padx = 5, pady = 5)
- 
-my_entry2 = Entry(frame, width = 15)
-my_entry2.insert(0,'Contraseña')
-my_entry2.pack(padx = 5, pady = 5)
-
-button1 = Button(frame, text = "Ingresar")
-button1.pack(padx = 3, pady = 3)
- 
-root.title("Libreria")
+frm = ttk.Frame(root, padding=40)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Menubutton = (frm)
+textInputUser = StringVar()
+textInputPass = StringVar()
+inputUser = ttk.Entry(frm, textvariable=textInputUser).grid(column=0, row=1)
+inputPass = ttk.Entry(frm, textvariable=textInputPass).grid(column=0, row=3)
+ttk.Button(frm, text="Login", command=login_clicked).grid(column=0, row=4)
 root.mainloop()

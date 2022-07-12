f4994c79083ddd819d20a2feece0d869682c85da
José Velásquez <joseivecas@gmail.com>
2022-07-12 02:33:10 -0400

    Update libreria.sql

--- libreria.sql
+++ libreria.sql
@@ -54,9 +54,21 @@ FOREIGN KEY (id_destino) REFERENCES bodega(id_bodega),
 FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
+ALTER TABLE movimiento ADD COLUMN id_productos_movimiento INT NOT NULL;
+ALTER TABLE movimiento DROP COLUMN id_productos_movimiento;
+ALTER TABLE movimiento ADD FOREIGN KEY (id_productos_movimiento) REFERENCES productos_movimiento(id_productos_movimiento);
+ALTER TABLE producto ADD COLUMN autores INT NOT NULL;
+ALTER TABLE producto DROP COLUMN autores;
+ALTER TABLE producto ADD FOREIGN KEY (autores) REFERENCES registro(id_registro);
+ALTER TABLE producto DROP FOREIGN KEY producto_ibfk_3;
+ALTER TABLE movimiento DROP FOREIGN KEY movimiento_ibfk_4;
+
+
 drop table movimiento;
 drop table bodega;
 drop table lista_productos;
+drop table producto;
+drop table registro;
 alter table producto drop column cantidad;
 
 CREATE TABLE lista_productos(
@@ -65,6 +77,15 @@ id_producto INT NOT NULL,
 id_bodega INT NOT NULL,
 cantidad INT NOT NULL,
 FOREIGN KEY (id_bodega) REFERENCES bodega(id_bodega),
+FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+);
+
+CREATE TABLE productos_movimiento(
+id_productos_movimiento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id_movimiento INT NOT NULL,
+id_producto INT NOT NULL,
+cantidad INT NOT NULL,
+FOREIGN KEY (id_movimiento) REFERENCES movimiento(id_movimiento),
 FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
 );
 
@@ -79,9 +100,20 @@ INSERT INTO editorial (nombre_editorial) VALUES("Debate");
 INSERT INTO editorial (nombre_editorial) VALUES("Debolsillo");
 
 show tables;
+SELECT * FROM bodega;
+SELECT * FROM lista_productos;
+SELECT * FROM producto;
 
 INSERT INTO bodega () VALUES ();
 select * from editorial;
+
+SELECT lista_productos.id,
+producto.titulo,
+bodega.id_bodega
+FROM lista_productos, producto, bodega
+WHERE
+lista_productos.id_bodega = bodega.id_bodega and
+lista_productos.id_producto = producto.id_producto;
 
 SELECT producto.tipo_producto,
 producto.titulo,
@@ -93,6 +125,7 @@ producto.id_editorial = editorial.id_editorial;
 SELECT id_producto FROM producto WHERE titulo = 'La invasion'; 
 
 ALTER TABLE bodega AUTO_INCREMENT = 1;
+ALTER TABLE usuario ADD COLUMN nombre_usuario varchar(20) NOT NULL;
 DELETE FROM bodega; 
 
 INSERT INTO producto(titulo, tipo_producto, id_editorial, descripcion, cantidad) VALUES("La montaña mágica", "Libro", 7, "sanatorio", 1);

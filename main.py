40426d34e34726bc96d0b65954341d7e126e2818
José Velásquez <joseivecas@gmail.com>
2022-07-12 17:27:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import movimiento
-import producto
 from getpass import getpass
 
 import mysql.connector
@@ -59,6 +57,7 @@ try:
                     createBodega = 'INSERT INTO bodega () VALUES ();'
                     mycursor.execute(createBodega)
                     mydb.commit()
+                    print('Bodega creada exitosamente')
 
                 elif(optionMainMenu == '2'):
                     idBodega = input('Ingrese el id de la bodega: ')
@@ -70,6 +69,7 @@ try:
                         deleteBodega = 'DELETE FROM BODEGA WHERE id_bodega = {}'.format(idBodega)
                         mycursor.execute(deleteBodega)
                         mydb.commit()
+                        print('Bodega eliminada exitosamente')
 
                 elif(optionMainMenu == '3'):
                     createProducto = 'INSERT INTO producto(titulo, tipo_producto, id_editorial, descripcion) VALUES(%s, %s, %s, %s)'
@@ -104,16 +104,18 @@ try:
                     for i in range(int(cantidadAutores)):
                         idAutor = input('Ingrese id del autor: ')
                         mycursor.execute(createRegistro, (idProducto, idAutor))
-                    mycursor.commit()
+                    mydb.commit()
+                    print('El producto fue creado exitosamente')
                 
                 elif (optionMainMenu == '4'):
                     deleteProducto = 'DELETE FROM producto WHERE id_producto = %s'
                     searchProducto = 'SELECT id_producto FROM lista_productos WHERE id_producto = %s'
                     idProducto = input('Ingrese id del producto: ')
                     mycursor.execute(searchProducto, [idProducto])
-                    if ( mycursor.fetchone == None):
+                    if (mycursor.fetchone() == None):
                         mycursor.execute(deleteProducto, [idProducto])
                         mydb.commit()
+                        print('Producto eliminado exitosamente')
                     else:
                         print('El producto ya se encuentra asignado a una bodega, imposible eliminar')
 
@@ -124,6 +126,7 @@ try:
                     cantidad = input('Ingrese la cantidad: ')
                     mycursor.execute(asignProducto,(idBodega, idProducto, cantidad))
                     mydb.commit()
+                    print('El producto fue asignado exitosamente')
 
                 elif (optionMainMenu == '6'):
                     insertAutor = 'INSERT INTO autor (nombres, apellidos) VALUES(%s, %s)'
@@ -131,15 +134,17 @@ try:
                     apellidos = input('Ingrese los apellidos del autor: ')
                     mycursor.execute(insertAutor,(nombres, apellidos))
                     mydb.commit()
+                    print('El autor fue agregado correctamente')
 
                 elif (optionMainMenu == '7'):
-                    deleteAutor = 'DELETE FROM autor WHERE id_autor == %s'
+                    deleteAutor = 'DELETE FROM autor WHERE id_autor = %s'
                     searchAutor = 'SELECT id_autor FROM registro WHERE id_autor = %s'
                     idAutor = input('Ingrese el id del autor: ')
                     mycursor.execute(searchAutor, [idAutor])
-                    if ( mycursor.fetchone == None):
+                    if ( mycursor.fetchone() == None):
                         mycursor.execute(deleteAutor, [idAutor])
                         mydb.commit()
+                        print('El autor fue eliminado exitosamente')
                     else:
                         print('El autor ya se encuentra asignado a un producto. Imposible eliminar.')
 
@@ -148,15 +153,17 @@ try:
                     nombreEditorial = input('Ingrese el nombre de la editorial: ')
                     mycursor.execute(insertEditorial, [nombreEditorial])
                     mydb.commit()
+                    print('La editorial fue agregada exitosamente')
 
                 elif (optionMainMenu == '9'):
-                    deleteEditorial = 'DELETE FROM editorial WHERE id_editorial == %s'
+                    deleteEditorial = 'DELETE FROM editorial WHERE id_editorial = %s'
                     searchEditorial = 'SELECT id_editorial FROM producto WHERE id_editorial = %s'
-                    nombreEditorial = input('Ingrese el nombre de la editorial: ')
+                    idEditorial = input('Ingrese el id de la editorial: ')
                     mycursor.execute(searchEditorial, [idEditorial])
-                    if ( mycursor.fetchone == None):
-                        mycursor.execute(deleteEditorial, (nombreEditorial))
+                    if ( mycursor.fetchone() == None):
+                        mycursor.execute(deleteEditorial, [idEditorial])
                         mydb.commit()
+                        print('La editorial fue eliminada exitosamente')
                     else:
                         print('La editorial ya se encuentra asignada a un producto. Imposible eliminar.')
 
@@ -173,7 +180,12 @@ try:
                     insertListaProducto = 'INSERT INTO lista_productos (id_producto, id_bodega, cantidad) VALUES (%s, %s, %s)'
                     insertMovimiento = 'INSERT INTO movimiento (id_origen, id_destino, id_usuario) VALUES (%s, %s, %s)'
                     insertProductosMovimiento = 'INSERT INTO productos_movimiento (id_movimiento, id_producto, cantidad) VALUES (%s, %s, %s)'
-                    searchListaProducto = 'SELECT cantidad FROM lista_productos WHERE id_bodega = %s AND id_producto = %s'
+
+                    deleteListaProducto = 'DELETE FROM lista_productos WHERE id = %s'
+
+                    searchListaProducto = 'SELECT cantidad, id FROM lista_productos WHERE id_bodega = %s AND id_producto = %s'
+                    searchMovimiento = 'SELECT id_movimiento FROM movimiento ORDER BY id_movimiento DESC LIMIT 1'
+
                     alterCantidadProducto = 'UPDATE lista_productos SET cantidad = %s WHERE id_bodega = %s AND id_producto = %s'
 
                     idBodegaOrigen = input('Ingrese la bodegan de origen: ')
@@ -184,11 +196,15 @@ try:
                     for i in range(movimientos):
                         idProducto = input('Ingrese id del producto: ')
                         mycursor.execute(searchListaProducto, (idBodegaOrigen, idProducto))
+                        listaProducto = mycursor.fetchone()
                         cantidadMovida = int(input('Ingrese la cantidad a mover: '))
-                        cantidadTotal = int(mycursor.fetchone()[0]) - cantidadMovida
-                        mycursor.execute(alterCantidadProducto, (cantidadMovida, idBodegaOrigen, idProducto))
+                        cantidadEncontrada = int(listaProducto[0])
+                        cantidadTotal = cantidadEncontrada - cantidadMovida
+                        mycursor.execute(alterCantidadProducto, (cantidadTotal, idBodegaOrigen, idProducto))
+                        if (cantidadTotal < 1):
+                            mycursor.execute(deleteListaProducto, [listaProducto[1]])
+
                         mycursor.execute(searchListaProducto,(idBodegaDestino, idProducto))
-                        
                         listaProducto = mycursor.fetchone()  
                         if (listaProducto == None):
                             mycursor.execute(insertListaProducto, (idProducto, idBodegaDestino, cantidadTotal))
@@ -196,6 +212,14 @@ try:
                             cantidadTotal = cantidadMovida + int(listaProducto[0])
                             mycursor.execute(alterCantidadProducto,(cantidadTotal, idBodegaDestino, idProducto))
                     mydb.commit()
+
+                    mycursor.execute(searchMovimiento)
+                    idMovimiento = mycursor.fetchone()[0]
+
+                    mycursor.execute(insertProductosMovimiento, (idMovimiento, idProducto, cantidadTotal))
+                    mydb.commit()
+
+                    print('Los productos fueron movidos exitosamente')
                 else:
                     print("Hasta pronto")
                     break

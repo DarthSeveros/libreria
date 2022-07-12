1bc54353ab681b1254d7997288e4ae68087e1513
José Velásquez <joseivecas@gmail.com>
2022-07-12 15:14:45 -0400

    Update actions.sql

--- actions.sql
+++ actions.sql
@@ -21,6 +21,8 @@ INSERT INTO usuario (nombre_usuario, tipo_usuario, contrasena) VALUES ('joseivec
 INSERT INTO usuario (nombre_usuario, tipo_usuario, contrasena) VALUES ('taly', 'Bodeguero', '123abc');
 commit;
 
+INSERT INTO `registro` (`id_producto`, `id_autor`) VALUES ('1', '1');
+
 show tables;
 SELECT * FROM bodega;
 SELECT * FROM lista_productos;
@@ -47,7 +49,9 @@ producto.id_editorial = editorial.id_editorial;
 SELECT id_producto FROM producto WHERE titulo = 'La invasion'; 
 
 ALTER TABLE bodega AUTO_INCREMENT = 1;
+ALTER TABLE producto AUTO_INCREMENT = 1;
 ALTER TABLE usuario ADD COLUMN nombre_usuario varchar(20) NOT NULL;
-DELETE FROM bodega; 
+DELETE FROM bodega;
+DELETE FROM producto WHERE id_producto = 6; 
 
 INSERT INTO producto(titulo, tipo_producto, id_editorial, descripcion, cantidad) VALUES("La montaña mágica", "Libro", 7, "sanatorio", 1);
